73029c7c00585498fa63614b502e764c77f1cf02
Manfred Kraft <m.kraft@email.de>
2014-09-02 21:29:49 +0200

    Start Cleanup, NumPy Smooth

--- mainapp.py
+++ mainapp.py
@@ -24,7 +24,7 @@ class SleepApp:
 		while True:
 			now = time.time()
 			elapsed = now - lastTime
-			if elapsed >= 0.0005:
+			if elapsed >= 0.1:
 				lastTime = now
 				self.sensor.readData()
 				self.clock.render()
