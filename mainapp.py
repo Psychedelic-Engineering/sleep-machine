ebc873bef37b6b3045985ace1b3d4b7b5bec25c6
Manfred Kraft <m.kraft@email.de>
2014-09-04 12:16:25 +0200

    Beginn Scheduler

--- mainapp.py
+++ mainapp.py
@@ -14,10 +14,10 @@ class SleepApp:
 	def __init__(self):
 		self.quitting = False
 		Peripherals.init()
-		#self.sensor = Sensor(Peripherals.devices["Pillow"])
+		self.sensor = Sensor(Peripherals.devices["Pillow"])
 		#self.sensor = FakeSensor()
 		self.display = Display(320, 240)
-		#self.graph = Graph(self.display, self.sensor)
+		self.graph = Graph(self.display, self.sensor)
 		self.clock = Clock(self.display)
 		self.scheduler = Scheduler()
 
@@ -26,9 +26,9 @@ class SleepApp:
 	def start(self):
 		while True:
 			if self.scheduler.elapsed(0.02):
-				#self.sensor.readData()
+				self.sensor.readData()
 				self.clock.render()
-				#self.graph.render((1,2,3))
+				self.graph.render((1,2,3))
 				#self.scheduler.check()
 
 	def quit(self):
