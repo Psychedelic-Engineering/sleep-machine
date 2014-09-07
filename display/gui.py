37eaeec0f9b73998b6704650e9a0589746a0c9fa
Manfred Kraft <m.kraft@email.de>
2014-09-07 18:45:11 +0200

    Alpha

--- display/gui.py
+++ display/gui.py
@@ -12,9 +12,11 @@ class GUI():
 		self.fontColor = (0,0,0)
 		self.guiFont = pygame.font.Font(self.fontname, 14)
 		self.surface = pygame.Surface((self.width, self.height))
-		self.slider = (20,100,280,100)
+		self.slider1 = (20,50,280,50)
+		self.slider2 = (20,150,280,50)
 		self.close = (280,0,40,40)
-		self.lum = 0
+		self.lumWarm = 0
+		self.lumCold = 0
 		self.drawBG()
 
 	def drawBG(self):
@@ -23,36 +25,36 @@ class GUI():
 	def handleEvent(self, event):
 		if event.type == pygame.MOUSEMOTION:
 			x,y = event.pos
-			if self.insideRect(event.pos, self.slider):
-				self.lum = self.sliderPos(x, self.slider)
-				self.drawSlider()
-				self.onSetLight(self.lum)
+			if self.insideRect(event.pos, self.slider1):
+				self.lumWarm = self.sliderPos(x, self.slider1)
+				self.render()
+				self.onSetLight(self.lumWarm, self.lumCold)
+
+			if self.insideRect(event.pos, self.slider2):
+				self.lumCold = self.sliderPos(x, self.slider2)
+				self.render()
+				self.onSetLight(self.lumWarm, self.lumCold)
 
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			if self.insideRect(event.pos, self.close):
-				self.drawBG()
 				self.onClose()
-
-
 
 	def render(self):
 		self.drawBG()
 		self.drawClose()
-		self.drawSlider()
+		self.drawSlider(self.slider1, self.lumWarm)
+		self.drawSlider(self.slider2, self.lumCold)
 		self.display.screen.blit(self.surface, (0, 0))
 		pygame.display.flip()
-
 
 	def drawClose(self):
 		pygame.draw.rect(self.surface, (255,0,0), self.close, 2)
 
-	def drawSlider(self):
-		l, t, w, h = self.slider
-		pygame.draw.rect(self.surface, (255,0,0), self.slider, 2)
-		w = int(float(w) * self.lum)
+	def drawSlider(self, slider, value):
+		l, t, w, h = slider
+		pygame.draw.rect(self.surface, (255,0,0), slider, 2)
+		w = int(float(w) * value)
 		pygame.draw.rect(self.surface, (255,255,0), [l, t, w, h])
-
-
 
 	def insideRect(self, pos, rect):
 		x, y = pos
