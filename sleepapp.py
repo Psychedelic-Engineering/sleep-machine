5956e421cfd39070136586f34e69c3764ee81f2f
Manfred Kraft <m.kraft@email.de>
2015-03-23 00:31:07 +0100

    New changelist

--- sleepapp.py
+++ sleepapp.py
@@ -48,9 +48,9 @@ class SleepApp:
 
 		self.scheduler = Scheduler()
 		# ToDo: Alarme in config File, periodisch auslesen
-		self.scheduler.addAlarm("*", "7", "00", actions.switchRelais)
+		self.scheduler.addAlarm("*", "6", "45", actions.switchRelais)
 		#self.scheduler.addAlarm("*", "*", "2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58", actions.switchRelais)
-		self.scheduler.addAlarm("*", "0", "0", actions.switchRelais)
+		self.scheduler.addAlarm("*", "0", "35", actions.switchRelais)
 
 	def onButton(self, action):
 		if action == "close":
