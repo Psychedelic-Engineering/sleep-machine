da31401bd6b19c7c9b335634888a3cdc8da95349
Manfred Kraft <m.kraft@email.de>
2015-03-25 15:08:29 +0100

    New changelist

--- sleepapp.py
+++ sleepapp.py
@@ -52,7 +52,7 @@ class SleepApp:
 		#self.scheduler.addAlarm("*", "*", "2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58", actions.switchRelais)
 		self.scheduler.addAlarm("*", "0", "35", actions.switchRelais)
 
-		#actions.fallasleep(self)
+		actions.fallasleep(self)
 
 	def onButton(self, action):
 		if action == "close":
