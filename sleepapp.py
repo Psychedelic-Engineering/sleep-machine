6ebdcb461cbd38dd723ca68ca991b32d8f0bb6e3
Manfred Kraft <m.kraft@email.de>
2015-04-03 00:11:39 +0200

    New changelist

--- sleepapp.py
+++ sleepapp.py
@@ -48,7 +48,7 @@ class SleepApp:
 
 		self.scheduler = Scheduler()
 		# ToDo: Alarme in config File, periodisch auslesen
-		self.scheduler.addAlarm("*", "6", "20", actions.switchRelais)
+		self.scheduler.addAlarm("*", "8", "0", actions.switchRelais)
 		#self.scheduler.addAlarm("*", "*", "2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58", actions.switchRelais)
 		self.scheduler.addAlarm("*", "21", "17", actions.switchRelais)
 
